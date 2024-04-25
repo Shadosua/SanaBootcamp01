34b32387dece89b334654cba698d1ecd49946b13
Олександр Пилявський <vt231_poi@student.ztu.edu.ua>
2024-04-25 13:02:02 +0300

    SanaBootcamp01

---
+++ FirstProgram/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstProgram.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
